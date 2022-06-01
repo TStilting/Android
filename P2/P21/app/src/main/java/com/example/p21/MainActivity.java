d10a46bfe8975f8498fdc49278a8be6fce06fde2
Troy <troy.stilting@st-2.com>
2022-06-01 11:13:01 +0200

    made P2.1 landscape only

--- P2/P21/app/src/main/java/com/example/p21/MainActivity.java
+++ P2/P21/app/src/main/java/com/example/p21/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.p21;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    public int counter_left = 0;
+    public int counter_right = 0;
+    Button button_left;
+    Button button_right;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button_left = (Button) findViewById(R.id.buttonLeft);
+        button_right = (Button) findViewById(R.id.buttonRight);
     }
 }
