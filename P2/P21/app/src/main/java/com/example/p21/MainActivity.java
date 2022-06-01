9ff76bc8609da4bcc56958b3e590bd7c49ad83c7
Troy <troy.stilting@st-2.com>
2022-06-01 14:46:17 +0200

    fixed a few things

--- P2/P21/app/src/main/java/com/example/p21/MainActivity.java
+++ P2/P21/app/src/main/java/com/example/p21/MainActivity.java
@@ -6,16 +6,17 @@ import android.os.Bundle;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    public int counter_left = 0;
-    public int counter_right = 0;
-    Button button_left;
-    Button button_right;
+    public int counter_left;
+    public int counter_right;
+    Button button_left = (Button) findViewById(R.id.buttonLeft);
+    Button button_right = (Button) findViewById(R.id.buttonRight);
+    Button button_reset = (Button) findViewById(R.id.buttonReset);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button_left = (Button) findViewById(R.id.buttonLeft);
-        button_right = (Button) findViewById(R.id.buttonRight);
+        button_left.setText(counter_left);
+        button_right.setText(counter_right);
     }
 }
