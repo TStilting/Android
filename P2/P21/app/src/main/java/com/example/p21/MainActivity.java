f1e74074fd61aead149311fd15936d4dd798a2c3
Troy <troy.stilting@st-2.com>
2022-06-07 14:42:19 +0200

    button works now

--- P2/P21/app/src/main/java/com/example/p21/MainActivity.java
+++ P2/P21/app/src/main/java/com/example/p21/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.p21;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,12 +9,26 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
     public int counter_left;
     public int counter_right;
-    String string_left = String.valueOf(counter_left);
-    String string_right = String.valueOf(counter_right);
+    String string_left = "" + counter_left;
+    String string_right = "" + counter_right;
+    Button buttonL;
+    Button buttonR;
+    Button reset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonL = findViewById(R.id.buttonLeft);
+        buttonR = findViewById(R.id.buttonRight);
+    }
+
+    public void pressedLeft(View button_left) {
+        buttonL.setText(string_left);
+        counter_left++;
+    }
+
+    public void pressedRight(View button_right) {
+
     }
 }
